41171381cebfda3656051488e505ed7103b3c83c
Luis Sandoval <luissandoval1126@gmail.com>
2020-02-17 21:28:40 -0800

    forcast is woring and displaying went back and cleared stuff that was duplicating

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var cityList = $('#cities')
 var submit = $("#submit");
 var cities = $('#cities');
-var apiKey= '92da633d3d15d688dad9bffac774f506'
+var apiKey= '166a433c57516f51dfab1f7edaed8413'
 var weatherUrl= 'https://api.openweathermap.org/data/2.5/'
 
 
@@ -10,7 +10,7 @@ var weatherUrl= 'https://api.openweathermap.org/data/2.5/'
 // all my functions set aside so I can focus on the Order.
 
 function getCities() {
-    let cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+    var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
 
     $('#cities').empty();
     cityList.forEach ( function (city) {
@@ -50,8 +50,8 @@ function weatherInfo(){
     updateCityList(inputCity);
     // Displays updated cityList
     getCities()
-    // current day call
-    var queryUrl = weatherUrl +'weather?q='+ inputCity + '&units=imperial&appid='+ apiKey
+    // Current day call
+    var queryUrl = `${weatherUrl}weather?q=${inputCity}&units=imperial&appid=${apiKey}`
        
     $.ajax({
         url: queryUrl,
@@ -71,7 +71,12 @@ function weatherInfo(){
         
     
     // 5 day Forcast Query
-    var queryUrl = weatherUrl +'forecast/daily?q='+ inputCity + '&units=imperial&appid='+ '166a433c57516f51dfab1f7edaed8413'
+    var queryUrl = `${weatherUrl}forecast/daily?q=${inputCity}&units=imperial&appid=${apiKey}`
+    // holds all the days
+    var weatherArr=[];
+    // holds data about each day
+    var forcast ={};
+    // gets the data for each day
     $.ajax({ 
         url: queryUrl,
         method:'GET'
@@ -80,22 +85,24 @@ function weatherInfo(){
             var days = response.list[i]
             forcast = {
               weather: days.weather[0].description,
-              icon: `http://openweathermap.org/img/w/days.weather[0].icon.png`,
+              icon: `http://openweathermap.org/img/w/${days.weather[0].icon}.png`,
               minTemp: days.temp.min,
               maxTemp: days.temp.max,
               humidity: days.humidity,
               date: moment(days.dt *1000).format('MM-DD_YYYY'),
             };
+            weatherArr.push(forcast);
             console.log(forcast)
         };
-       
+       forecast(weatherArr)
     })
    
  // closes function
 };
 function getUvIndex(response) {
     city = `lat=${parseInt(response.coord.lat)}&lon=${parseInt(response.coord.lon)}`;
-
+    // clears the old UV index
+    $('#curUv').empty()
     // set queryURL based on type of query
     requestType = 'uvi';
     query = `?${city}&appid=${apiKey}`;
@@ -125,7 +132,41 @@ function getUvIndex(response) {
       $('#curUv').append(color);
     }); 
   };
+  function forecast(forecast) {
+    // emptys out the old cards
+    $('#forecast').empty()
+// Builds the new cards
+    for  (i = 1; i < forecast.length; i++) {
+      var colmx1 = $('<div class="col mx-1">');
+      var cardBody = $('<div class="card-body forecast-card">');
+      var cardTitle = $('<h5 class="card-title">');
+      cardTitle.text(forecast[i].date);
+      var ul = $('<ul>');
 
+      var iconLi = $('<li>');
+      var iconI = $('<img>');
+        iconI.attr('src', forecast[i].icon);
+
+      var weathLi = $('<li>');
+      weathLi.text(forecast[i].weather);
+
+      var tempMinLi = $('<li>');
+      tempMinLi.text('Min Temp: ' + forecast[i].minTemp + " F");
+
+      var tempMaxLi = $('<li>');
+      tempMaxLi.text('Max Temp: ' + forecast[i].maxTemp + " F");
+
+      var humLi = $('<li>');
+      humLi.text('Humidity: ' + forecast[i].humidity + "%");
+
+      iconLi.append(iconI);
+      ul.append(iconLi).append(weathLi).append(tempMinLi).append(tempMaxLi).append(humLi);
+      cardTitle.append(ul);
+      cardBody.append(cardTitle);
+      colmx1.append(cardBody);
+      $('#forecast').append(colmx1);
+    }
+  };
 
 // stuff that happens in Order of happening
 //   two input meathods 
